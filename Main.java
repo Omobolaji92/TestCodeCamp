bb0aeafeb3a3966c795c80fce651bc9c0ad5759a
omobolaji92 <omobolaji92@gmail.com>
2021-12-03 09:09:49 +0100

    My first Java code

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World. This is my first java project!");
+    System.out.println("I'm so happy and excited to finally write my first code.");
+    System.out.println("I can count 1 to 100.");
+    System.out.println("I am passionate about Software Testing.");
+    }
+}
